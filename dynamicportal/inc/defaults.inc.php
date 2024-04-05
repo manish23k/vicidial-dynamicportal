8ff70bf84dae3d0153f3ff8421b2e000799bc5ae
ManishK <32355900+manish23k@users.noreply.github.com>
2024-04-05 16:22:29 +0530

    Update defaults.inc.php

--- dynamicportal/inc/defaults.inc.php
+++ dynamicportal/inc/defaults.inc.php
@@ -6,9 +6,9 @@ $PORTAL_secure=0; // 1 = Enable forced HTTPS, 0 = Disable forced HTTPS; If you a
 $PORTAL_logfile='debug.txt'; // Log file to write debug output to, does nothing otherwise
 $PORTAL_userlevel=1; // Minimum required ViciDial user level to enable dynamic portal authentication
 $PORTAL_topbar=1; // Whether to display the topbar with image or not
-$PORTAL_redirecturl='https://serverip-or-fqdn/agc/vicidial.php'; // X = Disabled, otherwise set to a url like https://server.ip/agc/vicidial.php
+$PORTAL_redirecturl='https://serverip-or-fqdn/vicidial/welcome.php'; // X = Disabled, otherwise set to a url like https://server.ip/agc/vicidial.php
 $PORTAL_redirectadmin='https://server-iporfqdn/vicidial/admin.php'; // Only matters if the above is not X and the valued of the $PORTAL_adminfield in vicidial_users equals 'admin'
-$PORTAL_redirectsecs=0; // How long to count down before redirecting in seconds
+$PORTAL_redirectsecs=65; // How long to count down before redirecting in seconds
 $PORTAL_redirectlogin=1; // 1 = Provide User/Phone Login, 0 = Do not provide User/Phone login
 $PORTAL_adminfield='phone_login'; // The field from vicidial_users to look for the word 'admin' in to determine redirect URL
 $PORTAL_incurdelay=1; // Seconds to delay ALL submit requests, 0=Disabled  (This is a brute-force counter measure)
